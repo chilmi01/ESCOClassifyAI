ba50131d69d25cf3f8bf79f2bf0fb197666ffe21
chilmi01 <hilmihacihuseyin@gmail.com>
2025-03-27 14:54:13 +0200

    Add files via upload

---
+++ app/esco-ui/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import ESCOClassifyAI from "./components/ESCOClassifyAI";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ESCOClassifyAI />);
