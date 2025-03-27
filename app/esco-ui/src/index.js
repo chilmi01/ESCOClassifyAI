3239e391e413938de637f77b01d4f5138818f29a
chilmi01 <hilmihacihuseyin@gmail.com>
2025-03-27 14:54:47 +0200

    Delete app/esco-ui directory

--- app/esco-ui/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import ESCOClassifyAI from "./components/ESCOClassifyAI";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ESCOClassifyAI />);
